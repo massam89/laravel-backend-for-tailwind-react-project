23769a76c0e144ac5a69267749d54fb8ff107a87
Masoud Sam <mas.sam1368@gmail.com>
2023-02-22 09:27:18 +0330

    remove cache from api

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('lscache:max-age=300;public');
+})->middleware('lscache:max-age=864000;public');
