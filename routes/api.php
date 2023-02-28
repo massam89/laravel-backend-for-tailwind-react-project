705dd7d6f50996ee96b3cf12d46e14f11f77b205
Masoud Sam <mas.sam1368@gmail.com>
2023-02-28 11:08:19 +0330

    change middleware

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware' => ['api', 'lscache:private;esi=on;max-age=120'],'prefix' => 'auth'], function () {
+Route::group(['middleware' => ['api', 'lscache:no-cache'],'prefix' => 'auth'], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
